5616367e246ea0da4654d36b923f4ab18b1cef4d
Andrei-Anaheim <86974515+Andrei-Anaheim@users.noreply.github.com>
2021-08-11 19:47:05 +0300

    add: code index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array && array.length >0) {
+        let x = Array.from(array).sort((o, p) => o - p);
+        return x[0];
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length >0) {
+        let x = Array.from(array).sort((o, p) => p - o);
+        return x[0];
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array && array.length >0) {
+        return array.reduce((a,b) => a+b,0)/array.length; 
+    } else {
+        return 0;
+    }
 }
